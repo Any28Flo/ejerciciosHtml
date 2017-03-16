16770e06402dea27e0bd1f7c9788a591c6102595
Any <foglzerika@gmail.com>
2017-03-16 17:09:29 +0000

    Conectando js

--- ejercisio2/js/convierteGrados.js
+++ ejercisio2/js/convierteGrados.js
@@ -1,1 +1,16 @@
-console.log("hola");
+function convierteGrados(){
+
+			var grados = document.getElementById("gradosCelcius").value;
+			console.log('grados');
+			console.log(grados);
+			var resultado= grados * 9 /5;
+			document.getElementById("resultado").value = resultado
+			//return 
+}
+function convierteFraren(){
+
+	var gradosFaren = document.getElementById("faren").value;
+	var resultado = ((gradosFaren - 32)* 5) / 9;
+
+	document.getElementById("resultado").value = resultado;
+}
